598080adea2bcd784f9c372102a341295b176388
Simon Dosda <sdosda@gmail.com>
2022-12-03 16:10:56 +0100

    feat: add bulma

--- pages/assets/new.tsx
+++ pages/assets/new.tsx
@@ -12,29 +12,64 @@ interface Props {
 export default function NewAsset({ markets }: Props) {
   return (
     <div>
-      <h2>Add new asset</h2>
+      <h2 className="title">Add new asset</h2>
       <form action="/api/assets/new" method="post">
-        <label htmlFor="name">Asset</label>
-        <input type="text" id="name" name="name" />
+        <div className="field">
+          <label className="label" htmlFor="name">
+            Asset
+          </label>
+          <input className="input" type="text" id="name" name="name" />
+        </div>
 
-        <label htmlFor="sector">Sector</label>
-        <select name="sector" id="sector">
-          {sectors.map((sector) => (
-            <option value={sector}>{sector}</option>
-          ))}
-        </select>
+        <div className="field">
+          <label className="label" htmlFor="market">
+            Market
+          </label>
+          <div className="select">
+            <select name="market" id="market">
+              {markets.map((market) => (
+                <option value={market.id} key={market.id}>
+                  {market.attributes.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
 
-        <label htmlFor="link">Link</label>
-        <input type="text" id="link" name="link" />
+        <div className="field">
+          <label className="label" htmlFor="sector">
+            Sector
+          </label>
+          <div className="select">
+            <select name="sector" id="sector">
+              {sectors.map((sector) => (
+                <option value={sector} key={sector}>
+                  {sector}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
 
-        <label htmlFor="market">Market</label>
-        <select name="market" id="market">
-          {markets.map((market) => (
-            <option value={market.id}>{market.attributes.name}</option>
-          ))}
-        </select>
+        <div className="field">
+          <label className="label" htmlFor="link">
+            Link
+          </label>
+          <input className="input" type="text" id="link" name="link" />
+        </div>
 
-        <button type="submit">Submit</button>
+        <div className="field is-grouped">
+          <div className="control">
+            <button type="submit" className="button is-link">
+              Submit
+            </button>
+          </div>
+          <div className="control">
+            <Link href="/assets" className="button is-link is-light">
+              Cancel
+            </Link>
+          </div>
+        </div>
       </form>
     </div>
   );
